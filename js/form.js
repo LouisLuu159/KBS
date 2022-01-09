7748734d1ec00c3527d7f88ad6630cbf3835ccef
louis <tung8c159@gmail.com>
2022-01-09 22:55:13 +0700

    Fixed index file and add validator

--- js/form.js
+++ js/form.js
@@ -1,24 +1,74 @@
-
-const list = []
+const list = [];
 
 const getID = () => {
-    const select = document.querySelectorAll(".symptom-list")
-    for(let i = 0; i < select.length; i++) {
-        const id = select[i].options[select[i].selectedIndex]
-        console.log(id);
-    }
-}
+  const select = document.querySelectorAll(".symptom-list");
+  for (let i = 0; i < select.length; i++) {
+    const id = select[i].options[select[i].selectedIndex];
+    console.log(id);
+  }
+};
 
 const handleSubmit = () => {
-    const info = []
+  const info = [];
 
-    const select = document.querySelectorAll(".symptom-list")
-    for(let i = 0; i < select.length; i++) {
-        const name = select[i].name
-        const value = select[i].value
-        const newObj = {name: name, id: value}
-        info.push(newObj)
+  const select = document.querySelectorAll(".symptom-list");
+  for (let i = 0; i < select.length; i++) {
+    const name = select[i].name;
+    const value = select[i].value;
+    const newObj = { name: name, id: value };
+    info.push(newObj);
+  }
+
+  console.log(info);
+};
+
+$(function () {
+  //   $.validator.addMethod(
+  //     "validInputSymptom",
+  //     function (value, element) {
+  //       if ($("input[name='showSymptom']:checked").val() == "Yes") {
+  //         if (value == "") return false;
+  //       }
+  //       return true;
+  //     },
+  //     "Bạn chưa nhập triệu chứng."
+  //   );
+  $("#form").validate({
+    rules: {
+      GanXuong: "required",
+      CoLuc: "required",
+    },
+    // Specify validation error messages
+    messages: {
+      GanXuong: "Bạn chưa chọn Phản xạ gân xương",
+      CoLuc: "Bạn chưa chọn Cơ Lực",
+    },
+
+    errorPlacement: function (error, element) {
+      error.insertAfter(element);
+    },
+
+    submitHandler: function (form) {
+      form.submit();
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+  });
+});
+
+$(document).ready(function () {
+  $("#camgiacnong").hide();
+  $("#camgiacsau").hide();
+  $("#camgiacBT").hide();
+  $("#camgiacdau").hide();
+  $("#roiloancg").change((event) => {
+    let RoiLoanCG = $("#roiloancg-dropdown").val();
+    console.log(typeof RoiLoanCG + ", val:" + RoiLoanCG);
+    if (RoiLoanCG === "1") {
+      $("#camgiacnong").show();
+      $("#camgiacsau").show();
+      $("#camgiacBT").show();
+      $("#camgiacdau").show();
     }
-    
-    console.log(info);
-}
+  });
+});
